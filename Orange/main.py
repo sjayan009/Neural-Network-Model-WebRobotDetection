8a78244de05f6cf40bdba434c46c9eec1a798881
sjayan009 <70182014+sjayan009@users.noreply.github.com>
2023-09-05 21:36:54 -0400

    Add files via upload

--- Orange/main.py
+++ Orange/main.py
@@ -6,11 +6,15 @@ Created on Sun Aug 13 10:41:26 2023
 """
 
 import pandas as pd
+import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from matplotlib import pyplot as plt
 
 import pickle
 
@@ -27,11 +31,10 @@ print(df.isnull().sum().sum())
 features = df.iloc[:, 0:30]
 target_var = df.iloc[:, 30]
 
-print(features)
-print(target_var)
-
+#Splitting Data into Train and Test
 X_train, X_test, y_train, y_test = train_test_split(features, target_var, test_size=0.2, random_state=0)
 
+#train_scores, test_scores = list(), list()
 
 #Sequential Model - With Layers
 model = Sequential()
@@ -41,14 +44,34 @@ model.add(Dense(30, activation='relu'))
 #model.add(Dropout(0.3))
 model.add(Dense(1, activation='sigmoid'))
 
+#Compiling Model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-hist = model.fit(X_train, y_train, epochs=25, batch_size=10)
 
-test_loss, test_acc = model.evaluate(X_test, y_test)
-print("Accuracy: ", test_acc)
+#Fitting Model into 'hist' variable
+hist = model.fit(X_train, y_train, epochs=50, batch_size=10, validation_data=(X_test, y_test))
 
-model.save('orange_nodropout', hist)
+#Evaluating on Train Dataset
+#train_mp = (model.predict(X_train) >= 0.5).astype("int")
+#train_acc_ov = accuracy_score(y_train, train_mp)
+#train_scores.append(train_acc_ov)
+
+#Evaluating on Test Dataset
+#test_mp = (model.predict(X_test) >= 0.5).astype("int")
+#test_acc_ov = accuracy_score(y_test, test_mp)
+#test_scores.append(test_acc_ov)
+
+
+plt.plot(hist.history['accuracy'], '-o', label='Train')
+plt.plot(hist.history['val_accuracy'], '-o', label='Test')
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.title("Epoch vs Accuracy")
+plt.legend()
+plt.show()
+
+#Saving Model
+model.save('Overfitting.keras', hist)
 
 with open("my_model.pkcls", "wb") as file:
     pickle.dump(model, file)
